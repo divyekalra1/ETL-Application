9db0ed67a561d6edfd05a1e978dfc86c6d41601a
Vippul Jangira <vippuljangira@gmail.com>
2021-07-04 22:30:14 +0530

    logging all exception statement added

--- extraction.py
+++ extraction.py
@@ -29,9 +29,11 @@ Base = declarative_base()
 def checkformat(file_path):
     if file_path.suffix == '.xlsx' or file_path.suffix == '.xls':
         frame = pd.read_excel(
-            f"extracted_files/{file_path.name}", engine='openpyxl')
+            f"extracted_files\{file_path.name}", engine='openpyxl')
+        logger.info("file format=excel")
     elif file_path.suffix == '.csv':
-        frame = pd.read_csv(f"extracted_files/{file_path.name}")
+        frame = pd.read_csv(f"extracted_files\{file_path.name}")
+        logger.info("file format=csv")
     # json file support to be added later
     # elif file_path.suffix == '.json':
     #     frame = pd.read_json(f"extracted_files/{file_path.name}")
@@ -39,12 +41,12 @@ def checkformat(file_path):
 
 
 if __name__ == "__main__":
-
     print("Creating Database...")
+    logger.info("CReating dataabse")
     engine = create_engine('sqlite:///ETL-database.db', echo=True)
     sqlite_connection = engine.connect()  # connection definition
     directory = Path(
-        "/home/divyekalra/Desktop/ETL-Application/extracted_files")
+        "D:\Workdesk\PS-1\ETL-Application\extracted_files")
     old_path = Path()
     while True:
         if not keyboard.is_pressed('c'):
@@ -64,15 +66,16 @@ if __name__ == "__main__":
                 except ValueError as ve:  # Raise ValueError if table already exists
                     print(ve)
                     logger.exception("-Error-")
+                except:
+                    logger.exception("-Error-")
                 old_path = file_path
 
         else:
             break
-    Base.metadata.create_all(engine)  # Issue CREATE TABLE statement
 
-    logger.info(engine.execute(".tables"))
+    Base.metadata.create_all(engine)  # Issue CREATE TABLE statement
 
     sqlite_connection.close()  # Close the connection
     engine.dispose()  # dispose of the engine
     logger.info('connection closed and engine disposed')
-    logger.info('Extraction completed')
+    logger.info('Extraction complete')
